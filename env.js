7b8a2cc7d2e662af21efe85e68b0ff56e2527240
Waviq Subhi <waviq.subkhi@gmail.com>
2018-09-07 19:04:37 +0700

    switch to live

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-// export const baseApiUrl = 'http://dev.waviq-tuty.com/api/';
-export const baseApiUrl = 'http://localhost:8000/api/';
+export const baseApiUrl = 'http://dev.waviq-tuty.com/api/';
+// export const baseApiUrl = 'http://localhost:8000/api/';
